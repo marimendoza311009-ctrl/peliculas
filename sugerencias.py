60413e657824c277b4198bc21318ac98e11b9ee9
marimendoza311009-ctrl <mari.mendoza311009@gmail.com>
2026-09-07 10:06:05 -0300

    Add files via upload

---
+++ sugerencias.py
@@ -0,0 +1,29 @@
+print("=============================\n")
+print("   Sugerencias de peliculas  \n")
+print("=============================\n")
+usuario=input("Buen dia, cual es tu nombre? ")
+print("¿Que queres ver hoy, "+usuario+"?")
+nombre_pelicula="Rapido Furioso"
+genero_pelicula="accion"
+anio_pelicula=2001
+rating_pelicula=6.8
+nombre_pelicula2="Troya"
+genero_pelicula2="accion"
+anio_pelicula2=2004
+rating_pelicula2=7.4
+nombre_pelicula3="Terminator"
+genero_pelicula3="accion"
+anio_pelicula3=1984
+rating_pelicula3=8.1
+nombre_pelicula4="¿Y donde esta el piloto?"
+genero_pelicula4="Comedia"
+anio_pelicula4=1980
+rating_pelicula4=7.7
+nombre_pelicula5="Son como niños"
+genero_pelicula5="Comedia"
+anio_pelicula5=2010 
+rating_pelicula=6
+print("----GENEROS----")
+print("ACCION")
+print("COMEDIA")
+genero_favorito=input("¿Que genero te gusta? ")
