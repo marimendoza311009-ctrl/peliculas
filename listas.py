11223b02cccfd591c2afccad8871e00df818f7fa
marimendoza311009-ctrl <mari.mendoza311009@gmail.com>
2026-09-18 10:59:32 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,8 @@
+nombre_planetas=["Tierra","Mercurio","Venus","Saturno","Luna",499,"Plutón",None,"Jupiter"]
+print(nombre_planetas)
+nombre_planetas.append("Sol")
+print(nombre_planetas)
+nombre_planetas.remove("Luna")
+print(nombre_planetas)
+nombre_planetas.remove("Sol")
+print(nombre_planetas)
